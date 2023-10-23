f2c0d16bd8f4eff59cc32530d5d13af01a9db6c9
VTI\duc.phamvan <kimconoc@gmail.com>
2023-10-23 17:40:03 +0700

    push code

--- Lottery.WebMvcCore/Lottery.WebMvc/Controllers/PhonebookController.cs
+++ Lottery.WebMvcCore/Lottery.WebMvc/Controllers/PhonebookController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lottery.DoMain.Constant;
+using Lottery.WebMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Lottery.WebMvc.Controllers
 {
@@ -11,6 +14,40 @@ namespace Lottery.WebMvc.Controllers
         public IActionResult CreatePlayer()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ExecuteCreatePlayer(string playerJsons)
+        {
+            var userData = GetCurrentUser();
+            var players = JsonConvert.DeserializeObject<List<PhonebookModel>>(playerJsons);
+            // Tạo mới
+            if (players[0].Id == null)
+            {
+                players[0].IsDeleted = false;
+                players[0].PhoneNumber = "";
+                var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
+                if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
+                {
+                    return Json(Success_Request(playerBase.Result.IsSuccessful));
+                }
+            }
+            // Update
+            else if (players[0].Id != null && !players[0].IsDeleted)
+            {
+                if (string.IsNullOrEmpty(players[0].PhoneNumber))
+                {
+                    players[0].PhoneNumber = "";
+                }
+                var playerBase = provider.PutAsync<object>(ApiUri.POST_UserUpdatePhonebook, players);
+                if (playerBase != null || playerBase.Result != null || playerBase.Result.IsSuccessful)
+                {
+                    return Json(Success_Request(playerBase.Result.IsSuccessful));
+                }
+            }
+
+            return View(Server_Error());
+
         }
     }
 }
