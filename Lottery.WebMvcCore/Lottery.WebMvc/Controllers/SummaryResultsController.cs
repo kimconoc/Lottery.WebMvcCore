72d65afd680d5f05bc3ebd796f712bfaee01bb0c
Admin <nguyenminhtamqn1992@gmail.com>
2023-11-05 12:44:32 +0700

    commit

--- Lottery.WebMvcCore/Lottery.WebMvc/Controllers/SummaryResultsController.cs
+++ Lottery.WebMvcCore/Lottery.WebMvc/Controllers/SummaryResultsController.cs
@@ -31,6 +31,26 @@ namespace Lottery.WebMvc.Controllers
             return View(dic);
         }
 
+        public IActionResult SummaryManyDay(string strFromDate ,string strToDate)
+        {
+            DateTime fromDate = Constant.ConvertStringToDateTime(strFromDate);
+            DateTime toDate = Constant.ConvertStringToDateTime(strToDate);
+            CountManyDayModel countManyDayModel = new CountManyDayModel()
+            {
+                FromDate = fromDate,
+                ToDate = toDate,
+                UserID = GetCurrentUser().Id,
+            };
+
+            var dataBase = provider.PostAsync<Dictionary<DateTime, List<CountByDay>>>(ApiUri.POST_HandlMessageCountManyDay, countManyDayModel);
+            if (dataBase == null || dataBase.Result == null || dataBase.Result.Data == null)
+            {
+                return Json(Server_Error("Đã có lỗi xảy ra!"));
+            }
+
+            return View(dataBase.Result.Data);
+        }
+
         public IActionResult GetPartialViewSummary(string dicSummaryModelJson)
         {
             var dicSummaryModel = JsonConvert.DeserializeObject<Dictionary<DateTime, List<CountByDay>>>(dicSummaryModelJson);
---
+++ Lottery.WebMvcCore/Lottery.WebMvc/Models/CountManyDayModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lottery.WebMvc.Models
+{
+    public class CountManyDayModel
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public int UserID { get; set; }
+    }
+}
