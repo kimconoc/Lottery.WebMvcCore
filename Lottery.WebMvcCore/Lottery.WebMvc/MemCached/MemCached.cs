1edc88c412168c04c5efd521e128227625f4ca33
VTI\duc.phamvan <kimconoc@gmail.com>
2023-11-06 18:18:24 +0700

    push code

---
+++ Lottery.WebMvcCore/Lottery.WebMvc/MemCached/MemCached.cs
@@ -0,0 +1,22 @@
+﻿using Lottery.DoMain.Constant;
+using Lottery.DoMain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lottery.WebMvc.MemCached
+{
+    public class MemCached
+    {
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+        //public MemCached(IHttpContextAccessor httpContextAccessor)
+        //{
+        //    _httpContextAccessor = httpContextAccessor;
+        //}
+        public static User GetCurrentUser(HttpContext httpContext)
+        {
+            var httpRequest = httpContext.Request;
+            var cookies = httpRequest.Cookies[Default.Get_Signin_Token];
+            return new User();
+        }
+    }
+}
