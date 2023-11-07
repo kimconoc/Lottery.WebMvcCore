aefc9945f4be9d0621328cb20f51dc45276a7407
VTI\duc.phamvan <kimconoc@gmail.com>
2023-11-07 15:07:10 +0700

    push

--- Lottery.WebMvcCore/Lottery.WebMvc/Program.cs
+++ Lottery.WebMvcCore/Lottery.WebMvc/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.IdleTimeout = TimeSpan.FromMinutes(15);
     options.Cookie.HttpOnly = false;
     options.Cookie.IsEssential = true;
 });
